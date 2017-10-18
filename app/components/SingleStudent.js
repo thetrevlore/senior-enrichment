14793c71505f6eec80581745bf5d113b3eaba9af
Trevor Storey <trevorstorey23@gmail.com>
2017-10-18 15:51:18 -0400

    Navigation and views done except campus images. Work on those.

--- app/components/SingleStudent.js
+++ app/components/SingleStudent.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-// import store from '../store';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class SingleStudent extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { student: {} };
+    this.state = { student: {}, studentCampus: {} };
   }
   componentDidMount() {
     const studentId = this.props.match.params.studentId
     axios.get(`/api/students/${studentId}`).then(res=>res.data)
-    .then(student=>this.setState({student}))
+    .then(student => {
+      this.setState({student})
+      return student.campusId;
+    })
+    .then((campusId) => {
+      axios.get(`/api/campuses/${campusId}`).then(res=>res.data)
+      .then(studentCampus=>this.setState({studentCampus}));
+    })
   }
 
   render() {
-    const student = this.state.student
+    const student = this.state.student;
+    const campus = this.state.studentCampus;
     return (
-      <ul><h2>{student.name}</h2>
-        <li><h3>{student.email}</h3></li>
-      </ul>
+      <div>
+        <ul><h2>{student.name}</h2>
+          <li><h4>{student.email}</h4></li>
+        </ul>
+        <h3>{`${student.name} goes to `}<Link to={`/campuses/${campus.id}`}>{`${campus.name}!`}</Link></h3>
+      </div>
     )
   }
 }
